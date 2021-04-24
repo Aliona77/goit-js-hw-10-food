f3f187ff99414ec2c2992bfb9cbe89ba3a5bda32
Aliona77 <chabanalona153@gmail.com>
2021-04-25 00:23:49 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import menuItem from './menu.json';
+
+//import menuTempate from './templates/tempates.hbs';
+
 import './styles.css';
+
+let menu = document.querySelector('.js-menu');
+
+function createMenu(array) {
+    const markup = array.map(post => menuItem(post)).join('');
+    menu.insertAdjacentHTML('beforeend', markup);
+}
+createMenu(menuItem);
