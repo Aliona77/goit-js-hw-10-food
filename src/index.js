2161255ec438f15f65934261dd74616e2cf5404e
Aliona77 <chabanalona153@gmail.com>
2021-04-26 20:49:59 +0300

    встаноила зміну теми світлої на темну

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import food from './menu.json';
 import './styles.css';
 
 
-///Розмітка 
+///                              Розмітка 
 
 const menu = document.querySelector('.js-menu');
      console.log(menu);
@@ -17,3 +17,30 @@ menu.insertAdjacentHTML('beforeend', markup);
 
 
 //                               ТЕМА
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+const body = document.querySelector('body');
+const input = document.getElementById('theme-switch-toggle');
+
+    function changeTheme() {
+        if (input.checked) {
+            body.classList.remove(Theme.LIGHT);
+            body.classList.add(Theme.DARK);
+            localStorage.setItem('Theme', Theme.DARK);
+            return;
+        }
+        body.classList.remove(Theme.DARK);
+        body.classList.add(Theme.LIGHT);
+        localStorage.setItem('Theme', Theme.LIGHT);
+    }
+
+    function themeMenu() {
+     if (localStorage.getItem('Theme') === 'dark-theme') {
+        body.classList.add(Theme.DARK);
+        input.checked = true;
+    }
+}
+input.addEventListener('click', changeTheme);
+themeMenu();
